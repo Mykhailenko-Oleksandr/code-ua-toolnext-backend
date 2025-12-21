8550ac5955f9b4756c1d16a3347f5819733bdc32
Sapper <saniamykhailenko@gmail.com>
2025-12-22 00:31:30 +0200

    add auth logout

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,12 +10,13 @@ import {
   loginUserSchema,
   registerSchema,
 } from "../validations/authValidation.js";
+import { authenticate } from "../middleware/authenticate.js";
 
 const router = Router();
 
 router.post("/api/auth/register", celebrate(registerSchema), registerUser);
 router.post("/api/auth/login", celebrate(loginUserSchema), loginUser);
-router.post("/api/auth/logout", logoutUser);
+router.post("/api/auth/logout", authenticate, logoutUser);
 router.post("/api/auth/refresh", refreshUserSession);
 
 export default router;
