52f4995be7592ba6b0b062132737b115cbf50d30
LanaStudio <196567654+vadim2807@users.noreply.github.com>
2025-12-27 19:06:13 +0200

    feat: feedbacks endpoints + optional populateFeedbacks

--- src/routes/feedbacksRoutes.js
+++ src/routes/feedbacksRoutes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
-import { getFeedbacksSchema } from "../validations/feedbacksValidation.js";
-import { getFeedbacks } from "../controllers/feedbacksControllers.js";
+import {
+  getFeedbacksSchema,
+  createFeedbackSchema,
+} from "../validations/feedbacksValidation.js";
+import { getFeedbacks, createFeedback } from "../controllers/feedbacksControllers.js";
+import { authenticate } from "../middleware/authenticate.js";
+
 const router = Router();
 
 router.get("/api/feedbacks", celebrate(getFeedbacksSchema), getFeedbacks);
+router.post(
+  "/api/feedbacks",
+  authenticate,
+  celebrate(createFeedbackSchema),
+  createFeedback,
+);
 
 export default router;
