e5553a10912b35f2fd7894b4a5dabf6973f73b8d
LanaStudio <196567654+vadim2807@users.noreply.github.com>
2025-12-28 19:23:27 +0200

    V2807: profile feedbacks

--- src/validations/usersValidation.js
+++ src/validations/usersValidation.js
@@ -24,3 +24,13 @@ export const userToolsSchema = {
     perPage: Joi.number().integer().min(5).max(20).default(8),
   }),
 };
+
+export const userFeedbacksSchema = {
+  [Segments.PARAMS]: Joi.object({
+    userId: Joi.string().custom(objectIdValidator).required(),
+  }),
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(30).default(15),
+  }),
+};
